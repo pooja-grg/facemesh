e91fe8619059952e576a86fbf94ce02feb27fd71
newbie060 <poozagrg432@gmail.com>
2025-12-27 19:13:38 +0545

    face landmark detection using tensorflowJs

--- src/App.js
+++ src/App.js
@@ -1,22 +1,90 @@
-import logo from './logo.svg';
 import './App.css';
+import { useRef, useEffect } from 'react';
+import * as tf from '@tensorflow/tfjs';
+import * as faceLandmarksDetection from '@tensorflow-models/face-landmarks-detection';
+import Webcam from 'react-webcam';
+import { drawMesh } from './utilities';
 
 function App() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+  const detectorRef = useRef(null);
+
+  useEffect(() => {
+    runFacemesh();
+  }, []);
+
+  const runFacemesh = async () => {
+    await tf.ready();
+
+    const model = faceLandmarksDetection.SupportedModels.MediaPipeFaceMesh;
+
+    const detector = await faceLandmarksDetection.createDetector(model, {
+      runtime: 'tfjs',
+      refineLandmarks: true,
+    });
+
+    detectorRef.current = detector;
+
+    setInterval(detect, 100);
+  };
+
+  const detect = async () => {
+    if (
+      webcamRef.current &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      const video = webcamRef.current.video;
+      const videoWidth = video.videoWidth;
+      const videoHeight = video.videoHeight;
+
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+
+      const faces = await detectorRef.current.estimateFaces(video);
+
+      const ctx = canvasRef.current.getContext('2d');
+      ctx.clearRect(0, 0, videoWidth, videoHeight);
+
+      drawMesh(faces, ctx);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Webcam
+          ref={webcamRef}
+          style={{
+            position: 'absolute',
+            marginLeft: 'auto',
+            marginRight: 'auto',
+            left: 0,
+            right: 0,
+            textAlign: 'center',
+            zIndex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
+
+        <canvas
+          ref={canvasRef}
+          style={{
+            position: 'absolute',
+            marginLeft: 'auto',
+            marginRight: 'auto',
+            left: 0,
+            right: 0,
+            textAlign: 'center',
+            zIndex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
       </header>
     </div>
   );
